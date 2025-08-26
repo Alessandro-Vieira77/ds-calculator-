2db1fbb0c53a3164612362902b69e62554680b89
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-26 10:38:59 -0300

    chore: added prettier and prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className=" bg-blue-600 p-6 text-center text-xl">Calculator</h1>
+      <h1 className="bg-blue-600 p-6 text-center text-xl">Calculator</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
