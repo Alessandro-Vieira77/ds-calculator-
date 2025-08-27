61dfc9280236f8eeb434cdac7f28d89a730f7f65
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-27 00:31:35 -0300

    feat:  add componets Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import Button from './components/Button'
+
 function App() {
   return (
     <div className="items-centerbg-lightGray my-5 flex min-h-screen w-full items-center justify-center">
-      <div className="w-ConteinerW bg-coalBlack h-ConteinerDadH flex flex-col justify-between">
+      <div className="w-ConteinerW bg-coalBlack min-h-ConteinerDadH flex flex-col justify-between">
         <div className="h-ConteinerDyplayH w-full"></div>
 
-        <div className="h-ConteinerButtonsH bg-lightGray w-full rounded-t-xl px-7 py-7">
-          <button className="w-buttonSize h-buttonSize bg-coldWhite text-sizeButton text-mediumBlue cursor-pointer rounded-2xl shadow-lg">
-            +
-          </button>
+        <div className="min-h-ConteinerButtonsH bg-coldWhite flex w-full flex-col items-center justify-between rounded-t-xl px-8 py-7">
+          <div className="border-brandWhite shadow- h-2 w-[129px] rounded-full border-3 shadow-md"></div>
+          <div className="flex w-full flex-wrap gap-5">
+            <Button color={'secundary'}>C</Button>
+            <Button color={'secundary'}>*/-</Button>
+            <Button color={'secundary'}>%</Button>
+            <Button color={'third'}>/</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button color={'third'}>x</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button color={'third'}>-</Button>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button color={'third'}>+</Button>
+            <Button width={'secundary'}>0</Button>
+            <Button>.</Button>
+            <Button bg={'secundary'} color={'first'}>
+              =
+            </Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,41 @@
+import { tv } from 'tailwind-variants'
+
+const Button = ({ children, color, bg, width }) => {
+  const button = tv({
+    base: 'cursor-pointer rounded-2xl px-5 py-4 text-4xl shadow-lg',
+    variants: {
+      bg: {
+        primary: 'bg-coldWhite',
+        secundary: 'bg-waterGreen',
+      },
+      color: {
+        primary: 'text-mediumBlue',
+        secundary: 'text-neutralGray',
+        third: 'text-bluishPurple',
+        first: 'text-brandWhite',
+      },
+      width: {
+        primary: 'w-buttonSize text-center',
+        secundary: 'w-[159.76px] text-left',
+      },
+    },
+    defaultVariants: {
+      color: 'primary',
+      bg: 'primary',
+      width: 'primary',
+    },
+  })
+
+  return (
+    <button
+      onClick={() => {
+        console.log(children)
+      }}
+      className={button({ bg, color, width: width })}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
