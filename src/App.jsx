762fa2630571e921fc04b7ed24d32930620ad5a8
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-26 22:41:15 -0300

    feat: calculator structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   return (
-    <div className="bg-lightGray h-screen w-full">
-      <button className="w-buttonSize h-buttonSize bg-coldWhite text-sizeButton text-mediumBlue m-5 rounded-2xl shadow-lg">
-        +
-      </button>
+    <div className="items-centerbg-lightGray my-5 flex min-h-screen w-full items-center justify-center">
+      <div className="w-ConteinerW bg-coalBlack h-ConteinerDadH flex flex-col justify-between">
+        <div className="h-ConteinerDyplayH w-full"></div>
+
+        <div className="h-ConteinerButtonsH bg-lightGray w-full rounded-t-xl px-7 py-7">
+          <button className="w-buttonSize h-buttonSize bg-coldWhite text-sizeButton text-mediumBlue cursor-pointer rounded-2xl shadow-lg">
+            +
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
